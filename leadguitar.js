91a6eae7dc2c61666379ffd063ec3ce6acc3e97d
LeadGuitarMx <icc.hernan@gmail.com>
2020-09-29 01:20:11 -0600

    Update leadguitar.js

--- leadguitar.js
+++ leadguitar.js
@@ -478,17 +478,12 @@ $('a[href*=".png"], a[href*=".gif"], a[href*=".jpg"], a[href*=".jpeg"]').addClas
 		});
 	}
 
-	// Prevent closing from click inside dropdown
-	$(document).on('click', '.dropdown-menu', function (e) {
-	  e.stopPropagation();
-	});
-
 	// make it as accordion for smaller screens
 	if ($(window).width() < 992) {
 		$('.submenu').hide();
 		$('.submenu .dropdown-menu').removeClass('dropdown-menu');
 		$('.dropdown-menu a').click(function(e){
-			e.preventDefault();
+			
 			  if($(this).next('.submenu').length){
 				$(this).next('.submenu').toggle();
 			  }
